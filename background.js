732af11c9bf28835a1b991a9edddd001fec4d85e
Dot <151170972+MrDotto@users.noreply.github.com>
2025-02-04 20:02:15 -0500

    Redacted API key

--- background.js
+++ background.js
@@ -30,7 +30,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
             headers: {
                 "accept": "application/json",
                 "content-type": "application/json",
-                "authorization": "Bearer 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"
+                "authorization": "Bearer [Redacted]"
             },
             method: "POST",
             body: JSON.stringify({
